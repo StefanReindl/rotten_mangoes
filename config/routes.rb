87fb75a6d906bf54ba051f3fea31ca58560f1935
Stefan Reindl <stefan.reindl@gmail.com>
2014-07-01 11:08:26 -0700

    step 7 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 RottenMangoes::Application.routes.draw do
+  get "users/new"
+  get "users/create"
   resources :movies
 
   root to: 'movies#index' 
+
+  resources :users, only: [:new, :create]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
